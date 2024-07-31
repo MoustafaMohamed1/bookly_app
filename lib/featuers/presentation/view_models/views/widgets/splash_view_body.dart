3690a514e6752545c75cbbfa0918ddc242997505
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-07-31 18:19:30 +0300

    spashView ui done

--- lib/featuers/presentation/view_models/views/widgets/splash_view_body.dart
+++ lib/featuers/presentation/view_models/views/widgets/splash_view_body.dart
@@ -1,9 +1,18 @@
+import 'package:bookly_app/core/utilis/assets.dart';
 import 'package:flutter/material.dart';
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+      ],
+
+
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:bookly_app/views/splash_view.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'featuers/presentation/view_models/views/splash_view.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -12,7 +14,11 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: SplashView(),
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
     );
   }
 }
