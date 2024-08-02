aae19761b3263bc7f50ece3cf9db6a8d2f40196c
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-08-02 14:51:28 +0300

    add cusomAppBar to home

---
+++ lib/featuers/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utilis/assets.dart';
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: EdgeInsets.symmetric(horizontal: 20,vertical: 40),
+      child: Row(
+
+        children: [
+          Image.asset(AssetsData.logo,height: 18,),
+          const Spacer(),
+          IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.magnifyingGlass,  ),)
+        ],
+      ),
+    );
+  }
+}
