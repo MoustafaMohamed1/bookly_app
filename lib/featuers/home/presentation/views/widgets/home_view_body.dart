8a50bb7173283554c2a4425dbd27f6859571821c
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-08-04 20:18:29 +0300

    best seller item part 3

--- lib/featuers/home/presentation/views/widgets/home_view_body.dart
+++ lib/featuers/home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,9 @@
-import 'package:bookly_app/core/utilis/assets.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utilis/styles.dart';
+import 'best_sller_list_view_item.dart';
 import 'custom_app_bar.dart';
 import 'featuerd_list_view.dart';
 
@@ -35,49 +36,4 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerHomeViewBody extends StatelessWidget {
-  const BestSellerHomeViewBody({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AssetsData.testImage,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  'Harry potter and the Goblet of Fire',
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
