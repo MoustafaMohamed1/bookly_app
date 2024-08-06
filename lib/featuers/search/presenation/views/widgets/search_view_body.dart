00f9c316454b4009e1309866f7289ea83357d19f
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-08-07 02:57:19 +0300

    create custom_text_field.dart

--- lib/featuers/search/presenation/views/widgets/search_view_body.dart
+++ lib/featuers/search/presenation/views/widgets/search_view_body.dart
@@ -1,12 +1,18 @@
-
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'custom_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [CustomSearchTextField()],
+        ));
   }
 }
+
