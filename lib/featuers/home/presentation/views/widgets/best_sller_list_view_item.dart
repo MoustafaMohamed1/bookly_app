923d8e377d5c8b9e5bba8f10ab771e36069e51d3
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-08-06 12:50:45 +0300

    best seller list view done

--- lib/featuers/home/presentation/views/widgets/best_sller_list_view_item.dart
+++ lib/featuers/home/presentation/views/widgets/best_sller_list_view_item.dart
@@ -1,65 +1,75 @@
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 import '../../../../../constants.dart';
 import '../../../../../core/utilis/assets.dart';
 import '../../../../../core/utilis/styles.dart';
-class BestSellerHomeViewBody extends StatelessWidget {
-  const BestSellerHomeViewBody({super.key});
+import 'book_rating.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding:  EdgeInsets.symmetric(horizontal: 30),child:SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AssetsData.testImage,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AssetsData.testImage,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'Harry potter and the Goblet of Fire',
-                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
+            const SizedBox(
+              width: 30,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  const Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle14,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '19.99 e',
+                        style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 3,
-              ),
-              const Text(
-                'J.K. Rowling',
-                style: Styles.textStyle14,
-              ),
-              Row(children: [
-                Text(
-                  '19.99 e',
-                  style:
-                  Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
-                ),
-              ])
-            ],
-          )
-        ],
+            ),
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
--- lib/featuers/home/presentation/views/widgets/home_view_body.dart
+++ lib/featuers/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../../../../../core/utilis/styles.dart';
-import 'best_sller_list_view_item.dart';
+import 'best_seler_list_view.dart';
 import 'custom_app_bar.dart';
 import 'featuerd_list_view.dart';
 
@@ -12,28 +10,34 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-        FeaturedBookListView(),
-        const SizedBox(
-          height: 50,
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: Text(
-            'Best Seller',
-            style: Styles.textStyle18,
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverFillRemaining(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomAppBar(),
+              const FeaturedBookListView(),
+              const SizedBox(
+                height: 50,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  'Best Seller',
+                  style: Styles.textStyle18,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const BestSellerListView(),
+            ],
           ),
         ),
-        SizedBox(
-          height: 20,
-        ),
-        BestSellerHomeViewBody(),
       ],
     );
   }
 }
-
 
