96551023938a4fdeb2685b73f8d05fe050f47821
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-08-06 22:08:28 +0300

    create simillar_box_list_view.dart

--- lib/featuers/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/featuers/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,9 +1,9 @@
 import 'package:bookly_app/core/utilis/styles.dart';
 import 'package:bookly_app/featuers/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/featuers/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:bookly_app/featuers/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly_app/featuers/home/presentation/views/widgets/simillar_box_list_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'books_action.dart';
 import 'custom_book_detailes_app_bar.dart';
 
@@ -13,44 +13,62 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        const CustomBookDetailsAppBar(),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * .26),
-          child: const CustomBookImage(),
-        ),
-        const SizedBox(
-          height: 43,
-        ),
-        const Text(
-          'The Jungle Book',
-          style: Styles.textStyle30,
-        ),
-        const SizedBox(
-          height: 6,
-        ),
-        Opacity(
-          opacity: .7,
-          child: Text(
-            'Rudyard Kipling',
-            style: Styles.textStyle18.copyWith(
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w500,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .26),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
-        ),
-        const SizedBox(
-          height: 12,
-        ),
-        const BookRating(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-       const SizedBox(height: 37,),
-       const  BooksAction(),
-      ],
+          const SizedBox(
+            height: 12,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          const SizedBox(
+            height: 37,
+          ),
+          const BooksAction(),
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child:Padding(padding: EdgeInsets.only(left: 8),
+            child:  Text(
+              'You Can also like',
+              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+            ),),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const SimillarBookListView(),
+          const SizedBox(height: 40,),
+        ],
+      ),
     );
   }
 }
-
-
--- lib/featuers/home/presentation/views/widgets/simillar_box_list_view.dart
+++ lib/featuers/home/presentation/views/widgets/simillar_box_list_view.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'custom_book_image.dart';
 
-class FeaturedBookListView extends StatelessWidget {
-  const FeaturedBookListView({super.key});
+import 'custom_book_image.dart';
+class SimillarBookListView extends StatelessWidget {
+  const SimillarBookListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * .23,
+      height: MediaQuery.of(context).size.height * .15,
       child: ListView.builder(itemCount: 10,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
             return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
+              padding: EdgeInsets.symmetric(horizontal: 5),
               child: CustomBookImage(),
             );
           }),
