b7a4a9bfa0ed8dc17853ab002d981553cf034878
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-08-06 15:34:14 +0300

    add title & subtitle to detailes

--- lib/featuers/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/featuers/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,20 +1,43 @@
+import 'package:bookly_app/core/utilis/styles.dart';
 import 'package:bookly_app/featuers/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_detailes_app_bar.dart';
+
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return
-     Column(
+    return Column(
       children: [
-        CustomBookDetailsAppBar(),
-        Padding(padding: EdgeInsets.symmetric(horizontal: width *.24),
-       child: CustomBookImage(),),
+        const CustomBookDetailsAppBar(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .26),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        const Text(
+          'The Jungle Book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
       ],
-     );
+    );
   }
 }
