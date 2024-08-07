ec7089403c172198e67025a824994ba3d3f0cc80
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-08-07 15:39:16 +0300

    create home repo metods and failuer file

--- lib/featuers/home/data/repos/home_repo.dart
+++ lib/featuers/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/errors/failuers.dart';
+import 'package:bookly_app/featuers/home/data/models/BookModel.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failuer, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failuer, List<BookModel>>> fetchFeaturedBooks();
 }
